7b9f33f71961fea7ad49786a543a1c52759f1693
Avichal Trivedi <avichaltrivedi111@gmail.com>
2024-10-13 19:32:15 +0530

    edits

--- index.js
+++ index.js
@@ -2,14 +2,13 @@
 import express from "express";
 import pg from "pg";
 import * as env from "dotenv";
-import bodyParser from "body-parser";
-import cors from "cors"; // Import cors
+import cors from "cors"; 
 
 const port = 3001;
 const app = express();
 env.config();
 
-// Use cors middleware
+
 app.use(cors());
 
 const db = new pg.Client({
@@ -19,42 +18,37 @@ const db = new pg.Client({
     password: process.env.PG_PASSWORD,
     port: process.env.PG_PORT,
 
-    // connectionString: process.env.POSTGRES_URL,
+    
 });
 
 db.connect();
 
-// Home route
 app.get("/", (req, res) => {
     res.send("Home");
 });
 
-// Fetch all godowns
+
 app.get('/api/godowns', async (req, res) => {
-    try {
-        const result = await db.query("SELECT * FROM godowns");
+    try {const result = await db.query("SELECT * FROM godowns");
         res.json(result.rows);
-    } catch (err) {
-        res.status(500).json({ error: err.message });
+    } catch (err) {res.status(500).json({ error: err.message });
     }
 });
 
-// Fetch items for a specific godown
+
 app.get('/api/items/:godownID', async (req, res) => {
     const { godownID } = req.params;
-    try {
-        const result = await db.query("SELECT * FROM items WHERE godown_id = $1", [godownID]);
+    try {const result = await db.query("SELECT * FROM items WHERE godown_id = $1", [godownID]);
         res.json(result.rows);
-    } catch (err) {
+    } catch (err)
+     {
         res.status(500).json({ error: err.message });
     }
 });
 
-// Fetch child godowns for a specific parent godown
 app.get('/api/child-godowns/:parentID', async (req, res) => {
     const { parentID } = req.params;
-    try {
-        const result = await db.query("SELECT * FROM godowns WHERE parent_id = $1", [parentID]);
+    try {const result = await db.query("SELECT * FROM godowns WHERE parent_id = $1", [parentID]);
         res.json(result.rows);
     } catch (err) {
         res.status(500).json({ error: err.message });
