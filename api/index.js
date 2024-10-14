1d33c3c7c5881f216627c2ec0e8ca5c903b53757
Avichal Trivedi <avichaltrivedi111@gmail.com>
2024-10-15 00:56:23 +0530

    deploy

--- api/index.js
+++ api/index.js
@@ -11,34 +11,37 @@ env.config();
 
 app.use(cors());
 
+
 const db = new pg.Client({
     user: process.env.PG_USER,
     host: process.env.PG_HOST,
     database: process.env.PG_DATABASE,
     password: process.env.PG_PASSWORD,
     port: process.env.PG_PORT,
-
+    ssl: {
+        rejectUnauthorized: false, // For self-signed certificates (common on hosted platforms like Aiven)
+    }
     
 });
 
 db.connect();
 
 app.get("/", (req, res) => {
-    res.send("Home");
+    res.send("This is default route");
 });
 
 
 app.get('/api/godowns', async (req, res) => {
-    try {const result = await db.query("SELECT * FROM godowns");
+    try {const result = await db.query(`SELECT * FROM "Godown"`);
         res.json(result.rows);
-    } catch (err) {res.status(500).json({ error: err.message });
+    } catch (err) {console.log(err);res.status(500).json({ error: err.message });
     }
 });
 
 
 app.get('/api/items/:godownID', async (req, res) => {
     const { godownID } = req.params;
-    try {const result = await db.query("SELECT * FROM items WHERE godown_id = $1", [godownID]);
+    try {const result = await db.query("SELECT * FROM \"Item\" WHERE godown_id = $1", [godownID]);
         res.json(result.rows);
     } catch (err)
      {
@@ -48,7 +51,7 @@ app.get('/api/items/:godownID', async (req, res) => {
 
 app.get('/api/child-godowns/:parentID', async (req, res) => {
     const { parentID } = req.params;
-    try {const result = await db.query("SELECT * FROM godowns WHERE parent_id = $1", [parentID]);
+    try {const result = await db.query("SELECT * FROM \"Godown\" WHERE parent_id = $1", [parentID]);
         res.json(result.rows);
     } catch (err) {
         res.status(500).json({ error: err.message });
@@ -58,3 +61,6 @@ app.get('/api/child-godowns/:parentID', async (req, res) => {
 app.listen(port, () => {
     console.log(`app listening on port ${port}...`);
 });
+
+
+export default app;
